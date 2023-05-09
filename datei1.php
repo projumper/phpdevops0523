e2c65f1b41914fd6100aa2514e922640034ea6dd
Your Name <you@example.com>
2023-05-09 13:30:28 +0200

    Variablen Datentypen

--- datei1.php
+++ datei1.php
@@ -1,5 +1,31 @@
 <?php
+//int
+$a = 5;
 
-echo "Hallo Welt";
+//string
+$b = "Hallo";
 
+//char
+$c = "w";
+
+//float
+$d = 3.4;
+
+//boolean
+$e = true;
+
+//array
+$f = array(1,2,3,4);
+
+echo $a . "\n";
+
+print_r($b . "\n");
+
+var_dump($c);
+
+echo $d . "\n";
+
+print_r($e . "\n");
+
+var_dump($f);
 

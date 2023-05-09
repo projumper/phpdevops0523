7be3fa42517c388b52e44e5b06ab3ec1026b0f07
Valerian1988 <vkleinschnitz@gmail.com>
2023-05-09 14:00:40 +0200

    hello valerian

---
+++ Valerian/variable.php
@@ -0,0 +1,17 @@
+<?php
+$x = 1;
+$y = 1.1;
+$a = "hello world";
+$b = array(1,2);
+$c = true; $d = false;
+
+//Ausgabe
+//echo "Hello World";
+//print_r($x); print_r("string"); 
+//var_dump($x); var_dump("string"); 
+
+echo $y."--".$a.$c;
+//var_dump($x);
+//print_r($x);
+//var_dump($b);
+var_dump($b[1]);

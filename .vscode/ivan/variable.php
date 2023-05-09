3492aaf5cd720da39e8503039c810afb234988a3
Your Name <you@example.com>
2023-05-09 13:17:09 +0200

    variablen & datentypen

---
+++ .vscode/ivan/variable.php
@@ -0,0 +1,63 @@
+<?php
+//https://www.php.net/
+
+
+//Kommentar
+
+/*
+*
+*mehr zeilliger Kommentar
+*
+*/
+
+#alter Kommentar (depricated)
+
+//TODO Aufgabe
+
+
+//Variabel
+
+//$1212IvanGartsev = 0 
+$IvanGartsev1212 = "aaaaa";
+$ivangartsev1212 = "bbbbb";
+
+$name = "Wert";
+$x = 11;
+$y = 12;
+
+//Datentyp
+//in php möglcih
+$x = 0;
+$x = "zeichenkette";
+$x = 1.2;
+$x = array();
+
+//integer - natürlcih Zahlen
+$x = 10;
+
+//string
+$x = "zeichenkette";
+
+//char
+$y = "z";
+
+//float - kommazahl
+$x = 1.2;
+
+//bool
+$x = true; $x = false;
+
+//array
+$x = array(0,1,2,3,4,5,6,7,8,9);
+$yy = array(0,"zeichenkette","c",3.3,4,5,6,7,array(1,2,3,4),9);
+
+
+//object - oop
+
+
+//Ausgabe
+//echo "Hello World";
+//print_r($x); print_r("string"); 
+//var_dump($x); var_dump("string"); 
+
+//var_dump($yy);

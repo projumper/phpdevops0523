912385120d80a7f5e1617fb523e0dcbd5dbd4658
Your Name <you@example.com>
2023-05-25 15:07:29 +0200

    image css

--- ilka/page/header.php
+++ ilka/page/header.php
@@ -8,7 +8,7 @@
     <table width="100%">
         <tr>
             <td>
-            <img src="logo.jpg" widht="30">
+            <img src="logo.jpg" class="image">
             </td>
             <td align="right">
             <div class="navbar">

72fdf3f84ae41699077bb2402a56a79ae5eafd61
Your Name <you@example.com>
2023-05-25 15:50:53 +0200

    Bestätigung

--- ilka/page/index.php
+++ ilka/page/index.php
@@ -1,6 +1,11 @@
 <?php
 require_once("inhalt.php");
 require_once("header.php");
+
+if ($POST)
+{
+    echo "Ihre E-Mail-Adresse wurde eingetragen."
+}
 ?>
 <p><br></p>
 <?php
@@ -9,7 +14,7 @@ echo"<br><br>";
 ?>
 <form action="" method="post">
     E-Mail: <input type="text" name="mail"> 
-    <input type="submit" value="Senden">
+    <input type="submit" value="Abonnieren">
 </form>
 <p><br></p>
 <?php

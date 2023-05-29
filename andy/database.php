f0c5f07d3570eedf8b9b303d9c317b607e42bddc
Your Name <you@example.com>
2023-05-29 11:10:26 +0200

    edit

--- andy/database.php
+++ andy/database.php
@@ -2,9 +2,9 @@
 $servername = "awseb-e-uy7egtc6ti-stack-awsebrdsdatabase-icx5pfyyg3eo.ckfipzcqm9gp.eu-central-1.rds.amazonaws.com";  
 $username = "ebroot";  
 $password = "Prodigy02021983";  
+$dbname = "tasks"
 
-
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verbindung überprüfen:
 
@@ -12,5 +12,16 @@ if ($conn->connect_error) {
     die("Verbindung fehlgeschlagen: " . $conn->connect_error);
  }
 
-$sql = "CREATE DATABASE sqltest ";
+$sql = "CREATE TABLE `tasks`.`tasks` (`id` INT(11) NOT NULL AUTO_INCREMENT , `title` VARCHAR(255) NOT NULL , `description` TEXT NOT NULL , `comment` TEXT NOT NULL , `created_at` DATE NOT NULL , `updated_at` DATE NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB";
 $res = $conn->query($sql);
+
+
+if ($conn->query($sql) === TRUE) {
+echo "Tabelle 'users' erfolgreich erstellt.";
+} else {
+echo "Fehler beim Erstellen der Tabelle: " . $conn->error;
+}
+
+// Verbindung schließen:
+
+$conn->close();

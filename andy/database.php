27932bb13d932a026e2fe013c30c364881f87c57
Your Name <you@example.com>
2023-05-30 10:27:26 +0200

    edit

--- andy/database.php
+++ andy/database.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "awseb-e-uy7egtc6ti-stack-awsebrdsdatabase-icx5pfyyg3eo.ckfipzcqm9gp.eu-central-1.rds.amazonaws.com";  
-$username = "ebroot";  
-$password = "Prodigy02021983";  
+$servername = "localhost";  
+$username = "root";  
+$password = "";  
 
 
 $conn = new mysqli($servername, $username, $password);
